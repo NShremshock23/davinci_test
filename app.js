9c04f9eaca28efc126134ebd4c50e10367a5ab01
NShremshock23 <noahshremshock@gmail.com>
2023-05-23 11:08:46 -0400

    Simple POST demo

--- app.js
+++ app.js
@@ -3,19 +3,32 @@ const express = require('express');
 // express app
 const app = express();
 
+// Array to hold POST responses - TODO: replace with db?
+post_responses = [];
+
 // listen for requests
 app.listen(3000);
 
 // register view engine
 app.set('view engine', 'ejs');
 
+// Middleware, static files setup
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-    res.render('index', {title: 'Home'});
+    res.render('index', {title: 'Home', post_responses});
 });
 
 app.get('/post_demo', (req, res) => {
     res.render('post_demo', {title: 'POST Demo'});
 });
+
+app.post('/post_demo', (req, res) => {
+    console.log(req.body);
+    post_responses.push(req.body);
+    res.redirect('/');
+  });
 
 app.get('/socket_demo', (req, res) => {
     res.render('socket_demo', {title: 'Websockets Demo'});
